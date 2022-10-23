85d72bd09b67511a4b6cf61aa6a4b1ecfe9d28ea
Glindley23 <graham.lindley23@gmail.com>
2022-10-23 14:46:55 -0400

    added code from examples, encountering lots of errors

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,30 @@
 
 // Your Code Here
+const { response } = require("express");
+
+let response1 = await fetch('http://localhost:3001/listBooks' , {
+    method: 'GET',
+    headers: null,
+    body: null
+});
+
+let books = await response.json();
+console.log(books);
+
+
+let updateResponse = await fetch('http://localhost:9001/updateBook', {
+    method: "PATCH",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        'id': 3,
+        'title': 'Legends of Arathrae',
+    }),
+    
+});
+
+let updatedBook = await updateResponse.json();
+console.log(updatedBook);
+
+let 
